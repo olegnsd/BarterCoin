3966ea27a0334c0986b94950f12acfc78f44d216
guluzadeh <40172008+guluzadeh@users.noreply.github.com>
2020-10-08 23:11:21 +0400

    Create config.php

---
+++ inc/config.php
@@ -0,0 +1,9 @@
+<?
+
+$baseHref = '/bc2/';
+
+$mysql_conf['host'] = 'localhost';
+$mysql_conf['user'] = 'root';
+$mysql_conf['pass'] = '';
+$mysql_conf['dbname'] = 'barter';
+$mysql_conf['port'] = 3306;

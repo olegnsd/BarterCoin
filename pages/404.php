cfb512e3a6485f994c072a60738490fa4fc91489
guluzadeh <40172008+guluzadeh@users.noreply.github.com>
2020-10-08 23:12:19 +0400

    Create 404.php

---
+++ pages/404.php
@@ -0,0 +1,17 @@
+<div class="errorArea secPdng">
+	<div class="row errRow">
+		<div class="col-lg-5 col-lg-offset-1 col-md-6 col-md-offset-1">
+			<div class="errorContent">
+				<div class="h1 errorTitle">Ошибка 404!.</div>
+				<span>Страница не найдена!. <br>Вы можете перейти <a href="javascript:history.go(-1);">назад</a> или на <a href="<?=$baseHref;?>">главную страницу</a>.</span>
+				
+				<!--a href="#"><i class="icofont"></i> Go back to home</a-->
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-4 errCol">
+			<div class="eSearchImg">
+				<i class="fa fa-ban" style="    font-size: 16vw;"></i>
+			</div>
+		</div>
+	</div>
+</div>

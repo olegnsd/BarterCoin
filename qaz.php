c299e4a56029165dfa7a65b692457f2249dad312
olegnsd <info@holding.bz>
2020-10-26 22:14:47 +0300

    добавлено тестово

--- qaz.php
+++ qaz.php
@@ -1,3 +1,4 @@
 <?php
+echo "Привет мир!";
 #$_SERVER['REMOTE_ADDR']=$_SERVER['HTTP_X_REAL_IP'];                                                                                                                                          
 phpinfo();
